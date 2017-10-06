4235ee700f2e811e9610a7853818c0c7c200b584
choostanley <stanleychoo0522@hotmail.com>
2017-10-06 21:50:18 +0800

    Add association between Users and Notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
-  get 'welcome/index'
+  # get 'welcome/index'
+  root 'welcome#index'
 
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
   end
 
-  root "clearance/users#new"
+  # root "clearance/users#new"
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
